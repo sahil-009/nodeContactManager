0f32e9621546011a33e4fc10720250a87442bdd8
sahil-009 <sahilmund.6685@gmail.com>
2024-11-11 00:11:54 +0530

    router updated

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,8 +2,23 @@ const express = require("express")
 const router = express.Router()
 
 
-router.route("/",(req,res) =>{
-    res.status(200).json({message : "main Contacts route"})
+
+
+
+router.route("/").post((req,res) =>{
+    res.status(200).json({message : "contact created"})
+})
+
+router.route("/:id").get((req,res) =>{
+    res.status(200).json({message : `getting Contacts route ${req.params.id}`})
+})
+
+router.route("/:id").put((req,res) =>{
+    res.status(200).json({message : `"updated contacts ${req.params.id}`})
+})
+
+router.route("/").delete((req,res) =>{
+    res.status(200).json({message : `"main Contacts route ${req.params.id}`})
 })
 
 module.exports = router
